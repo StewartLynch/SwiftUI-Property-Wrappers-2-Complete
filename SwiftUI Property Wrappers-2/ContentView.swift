34122f8913e1c166de2567af96b8e82a0e6cd991
Stewart Lynch <slynch@createchsol.com>
2020-07-25 08:55:33 -0700

    Initial Commit

---
+++ SwiftUI Property Wrappers-2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI Property Wrappers-2
+//
+//  Created by Stewart Lynch on 2020-07-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Property Wrappers-2/SwiftUI_Property_Wrappers_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Property_Wrappers_2App.swift
+//  SwiftUI Property Wrappers-2
+//
+//  Created by Stewart Lynch on 2020-07-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Property_Wrappers_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
