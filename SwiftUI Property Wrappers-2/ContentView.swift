5f188018c927eedc567c07da357fbecd805bac70
Stewart Lynch <slynch@createchsol.com>
2020-08-20 11:56:35 -0700

    Verified with Xcode Beta 5

--- SwiftUI Property Wrappers-2/ContentView.swift
+++ SwiftUI Property Wrappers-2/ContentView.swift
@@ -43,7 +43,6 @@ struct ContentView: View {
                     NavigationLink(destination: NavDestinationView()) {
                         Text("Go Next")
                     }
-                    
                 }
             }
         }
